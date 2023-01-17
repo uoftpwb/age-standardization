1fbf9bc69701195a48b406df64c3d4570d144a23
makototakahara <113550542+makototakahara@users.noreply.github.com>
2023-01-17 15:26:24 -0500

    Update Replicating WHR Script.R

--- Replicating WHR Script.R
+++ Replicating WHR Script.R
@@ -1,27 +1,17 @@
 library(tidyverse)
 
 #Reading Gallup data
-gallup_main <- read.csv("/Users/MakBook/Downloads/GallupCleaned_Sep19.csv")
-gallup_main$Weighted.LS <- as.numeric(gallup_main$WGT*gallup_main$WP16.LS) 
+gallup_main <- read.csv(
+  "/Users/makototakahara/Downloads/GallupCleaned_Sep19.csv")
+gallup_main$Weighted.LS <- as.numeric(gallup_main$WGT*gallup_main$WP16.LS)
 
-#Finding unweighted life satisfaction scores, counting observations
+#Calculating life satisfaction scores, counting observations
 WHR_scores <- gallup_main %>% 
   filter(!is.na(Weighted.LS)) %>% 
   filter(YEAR_CALENDAR >= 2019 & YEAR_CALENDAR <= 2021) %>%
   group_by(COUNTRY_ISO3) %>% 
-  summarise(UW_Score = mean(Weighted.LS,na.rm=T),
-            n = n()) 
+  summarise(Weighted_Score = sum(Weighted.LS,na.rm=T)/sum(WGT, na.rm=T),
+            n = n())
 
-
-
-  
-
-
-
-
-
-
-test <- gallup_main %>%   
-  filter(!is.na(Weighted.LS)) %>%
-  filter(YEAR_CALENDAR == 2021) %>%
-  count(COUNTRY_ISO3)
+#Adding ranking column
+WHR_scores$Rank <- rank(-WHR_scores$Weighted_Score)
