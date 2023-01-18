e55d881ebd092f0325154bf61bac180a0d8709f6
Makoto Takahara <takaharian@gmail.com>
2023-01-18 15:42:56 -0500

    commit

--- Replicating WHR Script.R
+++ Replicating WHR Script.R
@@ -1,9 +1,10 @@
 library(tidyverse)
+library(plm)
 
 #Reading Gallup data
 gallup_main <- read.csv(
-  "/Users/makototakahara/Downloads/GallupCleaned_Sep19.csv")
-gallup_main$Weighted.LS <- as.numeric(gallup_main$WGT*gallup_main$WP16.LS)
+  "/Users/makototakahara/Downloads/PWB Lab/gallupCleaned230118_Makoto.csv")
+gallup_main$Weighted.LS <- as.numeric(gallup_main$WGT*gallup_main$WP16)
 
 #Calculating life satisfaction scores, counting observations
 WHR_scores <- gallup_main %>% 
@@ -18,8 +19,55 @@ WHR_scores$Rank <- rank(-WHR_scores$Weighted_Score)
 
 #TODO: Find Data for Six Factors:
 # 1. gdp - Log GDP per Capita
-# 2. ss - Social Support
-# 3.  - Healthy Life Expentency at Birth
-# 4.  - Freedom to Make Life Choices
-# 5.  - Generosity
-# 6.  - Perceptions of Corruption
+
+
+# 2. support - Social Support
+gallup_main$Weighted.support <- as.numeric(gallup_main$WGT*gallup_main$WP27)
+support <- gallup_main %>% 
+  filter(YEAR_CALENDAR >= 2019 & YEAR_CALENDAR <= 2021) %>%
+  group_by(COUNTRY_ISO3) %>% 
+  summarise(support = sum(Weighted.support,na.rm=T)/sum(WGT, na.rm=T)) %>% 
+  select(COUNTRY_ISO3, support)
+
+# 3. life_expectancy - Healthy Life Expectancy at Birth
+
+
+# 4. freedom - Freedom to Make Life Choices
+gallup_main$Weighted.freedom <- as.numeric(gallup_main$WGT*gallup_main$WP134)
+freedom <- gallup_main %>% 
+  filter(YEAR_CALENDAR >= 2019 & YEAR_CALENDAR <= 2021) %>%
+  group_by(COUNTRY_ISO3) %>% 
+  summarise(freedom = sum(Weighted.freedom,na.rm=T)/sum(WGT, na.rm=T)) %>% 
+  select(COUNTRY_ISO3, freedom)
+
+# 5. generosity - Generosity
+
+
+# 6. corruption - Perceptions of Corruption
+gallup_main$Weighted.bcorruption <- as.numeric(gallup_main$WGT*gallup_main$WP145)
+gallup_main$Weighted.gcorruption <- as.numeric(gallup_main$WGT*gallup_main$WP146)
+
+bcorruption <- gallup_main %>% 
+  filter(YEAR_CALENDAR >= 2019 & YEAR_CALENDAR <= 2021) %>%
+  group_by(COUNTRY_ISO3) %>% 
+  summarise(bcorruption = sum(Weighted.bcorruption,na.rm=T)/sum(WGT, na.rm=T),
+            nb=n()) %>% 
+  select(COUNTRY_ISO3, bcorruption, n)
+
+gcorruption <- gallup_main %>% 
+  filter(YEAR_CALENDAR >= 2019 & YEAR_CALENDAR <= 2021) %>%
+  group_by(COUNTRY_ISO3) %>% 
+  summarise(gcorruption = sum(Weighted.gcorruption,na.rm=T)/sum(WGT, na.rm=T),
+            ng=n()) %>% 
+  select(COUNTRY_ISO3, gcorruption, n)
+
+corruption <- merge(bcorruption, gcorruption, by="COUNTRY_ISO3") 
+  
+
+#Merging six factors into WHR_Scores df
+
+#PLM
+plm(WHR_scores$Weighted_Score ~ 
+      gdp+support+life_expectancy+freedom+generosity+corruption, 
+    data = "WHAT", model = "pooling")
+
