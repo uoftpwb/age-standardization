0e60aac903ca551561b33f1ca472c2de514a1420
Makoto Takahara <takaharian@gmail.com>
2023-01-27 17:19:44 -0500

    commit

--- Replicating WHR Script.R
+++ Replicating WHR Script.R
@@ -1,10 +1,12 @@
 library(tidyverse)
 library(plm)
+library(countrycode)
 
 #Reading Gallup data
 gallup_main <- read.csv(
   "/Users/makototakahara/Downloads/PWB Lab/gallupCleaned230118_Makoto.csv")
 gallup_main$Weighted.LS <- as.numeric(gallup_main$WGT*gallup_main$WP16)
+
 
 #Calculating life satisfaction scores, counting observations
 WHR_scores <- gallup_main %>% 
@@ -14,17 +16,31 @@ WHR_scores <- gallup_main %>%
   summarise(Weighted_Score = sum(Weighted.LS,na.rm=T)/sum(WGT, na.rm=T),
             n = n())
 
+
 #Adding ranking column
 WHR_scores$Rank <- rank(-WHR_scores$Weighted_Score)
+
 
 # 1. gdp - Log GDP per Capita
 gdp <- read.csv("/Users/makototakahara/Downloads/PWB Lab/gdp_per_capita.csv")
 gdp <- gdp %>% 
   select(Country.Code, X2019, X2020)
-growth <- read.csv(
+growth1 <- read.csv(
   "/Users/makototakahara/Downloads/PWB Lab/econ_growth_2021.csv")
+growth1 <- growth %>% 
+  select(LOCATION, Country, Value)
 growth2 <- read.csv(
   "/Users/makototakahara/Downloads/PWB Lab/growth2.csv")
+
+growth2$LOCATION <- countrycode(
+  growth2$Country,
+  "country.name",
+  "iso3c")
+growth2 <- growth2[, c(3, 1, 2)]
+
+growth <- rbind(growth1, growth2)
+growth <- unique(growth[ , c("LOCATION") ] )
+
 
 # 2. support - Social Support
 gallup_main$Weighted.support <- as.numeric(gallup_main$WGT*gallup_main$WP27)
@@ -33,9 +49,11 @@ support <- gallup_main %>%
   group_by(COUNTRY_ISO3) %>% 
   summarise(support = sum(Weighted.support,na.rm=T)/sum(WGT, na.rm=T)) %>% 
   select(COUNTRY_ISO3, support)
-WHR_scores$support <- support$support
+WHR_scores <- merge(WHR_scores, support, by = "COUNTRY_ISO3")
+
 
 # 3. life_expectancy - Healthy Life Expectancy at Birth
+
 
 
 # 4. freedom - Freedom to Make Life Choices
@@ -45,9 +63,11 @@ freedom <- gallup_main %>%
   group_by(COUNTRY_ISO3) %>% 
   summarise(freedom = sum(Weighted.freedom,na.rm=T)/sum(WGT, na.rm=T)) %>% 
   select(COUNTRY_ISO3, freedom)
-WHR_scores$freedom <- freedom$freedom
+WHR_scores <- merge(WHR_scores, freedom, by = "COUNTRY_ISO3")
+
 
 # 5. generosity - Generosity
+
 
 
 # 6. corruption - Perceptions of Corruption
@@ -71,12 +91,14 @@ gcorruption[gcorruption == 0] <- NA
 
 corruption <- merge(bcorruption, gcorruption, by="COUNTRY_ISO3") %>% 
   select(COUNTRY_ISO3, bcorruption, gcorruption)
-WHR_scores$corruption <- rowMeans(corruption[c("bcorruption", "gcorruption")], 
+corruption$corruption <- rowMeans(corruption[c("bcorruption", "gcorruption")], 
                                   na.rm=TRUE)
+corruption <- corruption %>% 
+  select("COUNTRY_ISO3", "corruption")
+WHR_scores <- merge(WHR_scores, corruption, by = "COUNTRY_ISO3")
+
 
 #PLM
 #plm(WHR_scores$Weighted_Score ~ 
 #      gdp+support+life_expectancy+freedom+generosity+corruption, 
 #    data = "WHAT", model = "pooling")
-
-
